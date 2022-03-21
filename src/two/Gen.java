784d9bffc8bc3e4e255135439e054a585de58546
Teleфonчиk <matvey-sizov@mail.ru>
2022-03-21 12:37:46 +0300

    Cleaning...

--- src/two/Gen.java
+++
@@ -1,9 +0,0 @@
-package two;
-
-import java.util.concurrent.ForkJoinPool;
-
-public class Gen {
-    public static void main(String[] args) {
-        new ForkJoinPool().invoke(new Setting());
-    }
-}
